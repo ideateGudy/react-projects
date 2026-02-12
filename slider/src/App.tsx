c1cc93196570f41bbb381466967d3a1f0bf45518
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-02-12 18:33:59 +0100

    feat: initialize project with main App component, styles, and SVG assets

---
+++ slider/src/App.tsx
@@ -0,0 +1,13 @@
+import BG from "./components/BG"
+import Slider from "./components/Slider"
+
+const App = () => {
+  return (
+    <div className="centerChild h-screen">
+      <BG />
+      <Slider/>
+    </div>
+  )
+}
+
+export default App
