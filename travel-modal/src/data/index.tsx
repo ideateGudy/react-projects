0748c18ac508ab66c8321391493a5b24f2e3bd3d
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-02-13 00:59:36 +0100

    feat: add image assets and flight offers data for Travel Modal

---
+++ travel-modal/src/data/index.tsx
@@ -0,0 +1,16 @@
+export const images: string[] = [
+  "images/img-1.jpg",
+  "images/img-2.jpg",
+  "images/img-3.jpg",
+  "images/img-4.jpg",
+  "images/img-5.jpg",
+];
+
+export const flightOffers: string[] = [
+  "✈️ 20% off on international flights – Limited time!",
+  "💺 Free seat upgrade on business class bookings.",
+  "🛄 Extra baggage allowance for frequent flyers.",
+  "🏨 Flight + Hotel combo deals with 30% savings.",
+  "🎟️ Buy one, get one free on select domestic routes.",
+  "🚀 Earn double loyalty points on all bookings this month.",
+];
