54329a07c9ad9bb2ed79ed8d7d0a20e318675432
Goodnews Azonubi <azonubigoodnews@gmail.com>
2026-02-12 15:52:04 +0100

    feat: add initial App and main entry files for the auth form

---
+++ auth-form/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import SignInSignUp from "./components/SignInSignUp";
+
+const App = () => {
+  return (
+    <div>
+      <SignInSignUp />
+    </div>
+  );
+};
+
+export default App;
